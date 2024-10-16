427dba5771dcb4da993ef54b59c0e7fdd7065621
Tad-awan, Zchiavoni S. <altertzchiavoni@gmail.com>
2024-10-16 12:46:50 +0800

    Made simple web with routing

--- Tad-awan_AP/src/app/app.component.ts
+++ Tad-awan_AP/src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { LayoutComponent } from './layout/layout.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent, BodyComponent,FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, ContentComponent,FooterComponent,LayoutComponent,UserDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css' 
 })
