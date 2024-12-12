caf6a2a7465c7d2c21a921b741c49db4bcb4c46c
EGGGDSY <you@example.com>
2024-12-13 02:39:51 +0800

    12/13/2024

--- Tad-awan_Finals_Project/src/app/class-record/class-record.component.ts
+++ Tad-awan_Finals_Project/src/app/class-record/class-record.component.ts
@@ -9,8 +9,11 @@ import { Record } from '../record';
 })
 export class ClassRecordComponent {
   records: Record[] = [
-    { id: 20225050, name: 'Zchiavoni Tad-awan', type: 'Homework', age: 20, grade: 'A', address: 'Baguio' },
-    { id: 20225051, name: 'John Doe', type: 'Assignment', age: 21, grade: 'B', address: 'Baguio' }
+    { id: 20225050, name: 'Zchiavoni Tad-awan', type: 'Homework', age: 20, grade: '78', address: 'Baguio' },
+    { id: 20225051, name: 'John Doe', type: 'Assignment', age: 21, grade: '90', address: 'Baguio' },
+    { id: 20225052, name: 'Juan Usa', type: 'Assignment', age: 22, grade: '84', address: 'Baguio' },
+    { id: 20225052, name: 'Juan Deer', type: 'Assignment', age: 24, grade: '80', address: 'Baguio' },
+    { id: 20225052, name: 'Juan John Jon Jan Jerry Joe', type: 'Assignment', age: 22, grade: '74', address: 'Baguio' },
   ];
 
   selectedRecord: Record | null = null;
